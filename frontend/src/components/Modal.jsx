15b034c2651db7e4e366e4c19bb7c5616365c458
Celine Bouhafs <celine.b.dev@gmail.com>
2024-10-18 12:59:18 +0200

    Delete unused code

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import Image from 'next/image';
 import '../styles/components/_modal.scss';
 
 const Modal = ({ onClose, showModal, children }) => {
