3d48f6ff3c43ba5594a343ba3c8ec559bfad57a3
Celine Bouhafs <celine.b.dev@gmail.com>
2024-10-18 14:28:34 +0200

    Add name

--- frontend/src/components/HeroHeader.jsx
+++ frontend/src/components/HeroHeader.jsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import '../styles/layout/_heroHeader.scss';
+import { Typewriter } from 'react-simple-typewriter';
 
 export default function HeroHeader() {
   return (
@@ -14,15 +15,30 @@ export default function HeroHeader() {
           priority
         />
       </div>
-      <div className='heroHeader_logo'>
+      <div className='heroHeader__logo'>
         <Image 
           src="/assets/logo_poupou.webp" 
           alt="Logo" 
-          width={600} 
-          height={600}
+          width={500} 
+          height={500}
           className="png-logo" 
           priority
+          sizes="(max-width: 480px) 40vw, (max-width: 1024px) 30vw, (max-width: 1200px) 20vw, 15vw" 
         />
+      </div>
+      <div className="heroHeader__text">
+        <h1 className="heroHeader__text__name">CÉLINE BOUHAFS</h1>
+        <div>
+          <Typewriter
+            words={['Développeuse Web']}
+            loop={false}
+            cursor
+            cursorStyle="_"
+            typeSpeed={100}
+            deleteSpeed={70}
+            delaySpeed={3000}
+          />
+        </div>
       </div>
     </div>
   );
